8c97293812848fb7836864b39fb01c162391efa4
mokrofka <mokrofka@gmail.com>
2026-05-06 21:23:10 +0300

    stop reallocating mem when resizing window

--- assets/shaders/defines/fragdef.glsl
+++
@@ -1,8 +0,0 @@
-layout (location = 0) in vec3 in_pos;
-layout (location = 1) in vec3 in_norm;
-layout (location = 2) in vec2 in_uv;
-layout (location = 3) in vec3 in_color;
-layout (location = 4) in flat uint in_entity_id;
-
-layout (location = 0) out vec4 out_color;
-
